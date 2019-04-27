43a88bd25be21e15bc9af35ba0e467acb04c453b
Progrema Studio <iqbalpakeh@gmail.com>
2019-04-28 06:32:47 +0700

    Add function and test

--- project-maven/my-app/src/main/java/com/mycompany/app/App.java
+++ project-maven/my-app/src/main/java/com/mycompany/app/App.java
@@ -4,10 +4,17 @@ package com.mycompany.app;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
     }
+
+    public int addNumbers(int a, int b) {
+        return a + b;
+    }
+
+    public int substractNumbers(int a, int b) {
+        return a - b;
+    }
+
 }
--- project-maven/my-app/src/test/java/com/mycompany/app/AppTest.java
+++ project-maven/my-app/src/test/java/com/mycompany/app/AppTest.java
@@ -7,14 +7,24 @@ import org.junit.Test;
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-{
+public class AppTest {
     /**
      * Rigorous Test :-)
      */
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+
+    @Test
+    public void addNumbersTest() {
+        App app = new App();
+        assertTrue(4 == app.addNumbers(2, 2));
+    }
+
+    @Test
+    public void substractNumbersTest() {
+        App app = new App();
+        assertTrue(2 == app.substractNumbers(4, 2));
     }
 }
