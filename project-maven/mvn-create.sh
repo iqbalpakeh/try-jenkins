1c25b3920cf34310809f858a917d2c59f20d7f1b
Progrema Studio <iqbalpakeh@gmail.com>
2019-04-29 06:29:49 +0700

    Add new create configuration

--- project-maven/mvn-create.sh
+++ project-maven/mvn-create.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
-mvn archetype:generate -DgroupId=com.mycompany.app -DartifactId=my-app -DarchetypeArtifactId=maven-archetype-quickstart -DarchetypeVersion=1.4 -DinteractiveMode=false
+#mvn archetype:generate -DgroupId=com.mycompany.app -DartifactId=my-app -DarchetypeArtifactId=maven-archetype-quickstart -DarchetypeVersion=1.4 -DinteractiveMode=false
+
+mvn archetype:generate -DgroupId=com.mkyong.hashing -DartifactId=my-app-2 -DarchetypeArtifactId=maven-archetype-quickstart -DinteractiveMode=false
